fc2cc4a28296d6900e88e2b979a82d4c4dae5346
Joey Adams <joeyadams3.14159@gmail.com>
2012-10-29 01:35:35 -0400

    testing/trivial.h: Use finally, so close after failed connect actually happens
    This reveals a race condition where CancelIo throws an exception.  We have to
    prevent CancelIo from executing after the socket is closed.

--- testing/trivial.hs
+++ testing/trivial.hs
@@ -6,8 +6,9 @@ import Network.Socket hiding (socket, connect)
 import Winsock
 
 import Control.Concurrent   (threadDelay)
-import Control.Monad    (forever)
-import System.Timeout   (timeout)
+import Control.Exception    (finally)
+import Control.Monad        (forever)
+import System.Timeout       (timeout)
 
 googleIP = "74.125.140.138"
 
@@ -16,8 +17,7 @@ main = do
         putStrLn "Connecting"
         sock <- socket AF_INET Stream defaultProtocol
         addr <- inet_addr googleIP
-        connect sock (SockAddrInet 1234 addr)
-        close sock
+        connect sock (SockAddrInet 1234 addr) `finally` close sock
 
         -- Avoid making successful connections repeatedly.
         threadDelay 10000000
