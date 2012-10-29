1a30c84e6e050fb5b0e01933de21003d0926f93a
Joey Adams <joeyadams3.14159@gmail.com>
2012-10-29 00:55:26 -0400

    Add simple program that repeatedly tries to connect to google.com:1234, timing it out

---
+++ testing/trivial.hs
@@ -0,0 +1,18 @@
+-- Compile with -threaded, or you'll get a pattern match failure.
+-- This library assumes a threaded runtime system.
+
+{-# OPTIONS -fno-warn-missing-signatures #-}
+import Network.Socket hiding (socket, connect)
+import Winsock
+
+import Control.Monad    (forever)
+import System.Timeout   (timeout)
+
+googleIP = "74.125.140.138"
+
+main = do
+    forever $ timeout 1000000 $ do
+        putStrLn "Connecting"
+        sock <- socket AF_INET Stream defaultProtocol
+        addr <- inet_addr googleIP
+        connect sock (SockAddrInet 1234 addr)
